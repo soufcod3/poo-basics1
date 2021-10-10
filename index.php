f3c46a0b61e70ceb2a0fdf1ecbd961b56f45d5e9
Soufiane Aït <89341670+soufcod3@users.noreply.github.com>
2021-10-10 10:14:08 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,35 +4,18 @@ require_once 'Car.php';
 
 
 $bike = new Bicycle();
-$newBike = new Bicycle();
 
-$car = new Car();
-$newCar = new Car();
+$car = new Car(rouge, 2, hybrid);
+
+echo '<strong>Test of the car:</strong>' . '<br>';
+echo $car->forward(92) . '<br>';
+echo $car->getCurrentSpeed() . '<br>';
+echo $car->brake();
+echo $car->getCurrentSpeed() . '<br><br>';
+
 
 echo "<strong>Test of the bike:</strong>" . '<br>';
 echo $bike->forward(25) . '<br>';
-echo "Vitesse du vélo: " . $bike->currentSpeed . ' km/h' . '<br>';
 echo $bike->brake();
 echo "Vitesse du vélo: " . $bike->currentSpeed . ' km/h' . '<br>';
 echo $bike->brake() . '<br><br>';
-
-echo "<strong>Test of the new bike:</strong>" . '<br>';
-echo $newBike->forward(37.2) . '<br>';
-echo "Vitesse du vélo: " . $newBike->currentSpeed . ' km/h' . '<br>';
-echo $newBike->brake();
-echo "Vitesse du vélo: " . $newBike->currentSpeed . ' km/h' . '<br>';
-echo $newBike->brake() . '<br><br>';
-
-echo "<strong>Test of the car:</strong>" . '<br>';
-echo $car->forward(92) . '<br>';
-echo "Vitesse de la voiture: " . $car->currentSpeed . ' km/h' . '<br>';
-echo $car->brake();
-echo "Vitesse de la voiture: " . $car->currentSpeed . ' km/h' . '<br>';
-echo $car->brake() . '<br><br>';
-
-echo "<strong>Test of the new car:</strong>" . '<br>';
-echo $newCar->forward(295) . '<br>';
-echo "Vitesse de la voiture: " . $newCar->currentSpeed . ' km/h' . '<br>';
-echo $newCar->brake();
-echo "Vitesse de la voiture: " . $newCar->currentSpeed . ' km/h' . '<br>';
-echo $newCar->brake();
